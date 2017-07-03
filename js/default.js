d206943f82d44767ebf8f9cc34f245f334bc4dd8
Nuyj <New.10_04@icloud.com>
2017-07-03 18:20:32 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -26,10 +26,10 @@ var refresh =  function(){
 update();
 
 function  getFileName() {
-  return  window.location.href.spilit('/').pop();
+  return  window.location.href.split('/').pop();
 }
 
-var filename  = getFileName();
+var filename = getFileName();
 var opt;
 if(filename === 'other.html'){
   opt = document.querySelector('option[value="other.html"]');
