a7e0cd36988fc17d2de06d387d78cd8a90504411
Nuyj <New.10_04@icloud.com>
2017-07-03 17:09:47 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,19 @@
+var separate_time = function(time){
+  var sec     = time.getSeconds();
+  var min     = time.getMinutes();
+  var hours   = time.getHours();
+  var days    = time.getDate();
+  var month   = time.getMonth();
+  var = year  = time.getfullYear();
+  return[sec, min,  hours,  days, month,  year];
+  }
+  
+  var now = new Date();
+  var counter = separate_time(now);
+document.getElementById('choice').textContent =
+  counter[5]  + '年' +
+  counter[5]  + '月' +
+  counter[5]  + '日' +
+  counter[5]  + '時' +
+  counter[5]  + '分' +
+  counter[5]  + '秒' ;
