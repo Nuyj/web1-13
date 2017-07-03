0401a9a536b3e067d0c74e60173566e4e06d8ba3
Nuyj <New.10_04@icloud.com>
2017-07-03 17:13:26 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -10,10 +10,10 @@ var separate_time = function(time){
   
   var now = new Date();
   var counter = separate_time(now);
-document.getElementById('choice').textContent =
+document.getElementById('countdown').textContent =
   counter[5]  + '年' +
-  counter[5]  + '月' +
-  counter[5]  + '日' +
-  counter[5]  + '時' +
-  counter[5]  + '分' +
-  counter[5]  + '秒' ;
+  counter[4]  + '月' +
+  counter[3]  + '日' +
+  counter[2]  + '時' +
+  counter[1]  + '分' +
+  counter[0]  + '秒' ;
